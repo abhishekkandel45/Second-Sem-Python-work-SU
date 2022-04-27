ff990bb779c3949cff60b80215c1deb7031cae8b
abhishekkandel45 <38427430+abhishekkandel45@users.noreply.github.com>
2022-04-27 13:37:59 +0530

    # Write the Program to Enter Names of Employees and their Salary as input and store them in a Dictionary. Print the Result

---
+++ Dictionary_Emp.py
@@ -0,0 +1,9 @@
+# Write the Program to Enter Names of Employees and their Salary as input and store them in a Dictionary. Print the Result
+
+name=input("Enter the name of the employee: ")
+salary=int(input("Enter the salary of the employee: "))
+
+emp={name:salary}
+print(emp)
+
+
