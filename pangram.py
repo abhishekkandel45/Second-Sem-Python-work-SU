39dcee829b6d156b383cd6f86ed5417d3a8cd0cb
abhishekkandel45 <38427430+abhishekkandel45@users.noreply.github.com>
2022-04-27 13:39:24 +0530

    # Write a program to check if a string is a pangram or not

--- pangram.py
+++ pangram.py
@@ -3,11 +3,10 @@ s=input("Enter the string: ")
 s=s.lower()
 alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 count=0
-for i in range(len(s)):
-    if s[i] in alphabets:
+for i in alphabets:
+    if i in s:
         count+=1
 if count==26:
-    print("The string is a pangram")
+    print("Pangram")
 else:
-    print("The string is not a pangram")    
-
+    print("Not a Pangram")
