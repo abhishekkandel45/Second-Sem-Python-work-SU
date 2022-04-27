700e63ddf7594a5a296d8d487d6dec1fcee55f9e
abhishekkandel45 <38427430+abhishekkandel45@users.noreply.github.com>
2022-04-27 13:38:33 +0530

    # Write a program to find the factorial of the given number  using interation.

---
+++ factorial.py
@@ -0,0 +1,12 @@
+# Write a program to find the factorial of the given number  using interation.
+num=int(input("Enter the number: "))
+fact=1
+if num<0:
+    print("Please enter a positive integer")
+elif num==0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1,num+1):
+        fact=fact*i
+    print("The factorial of",num,"is",fact)
+    
