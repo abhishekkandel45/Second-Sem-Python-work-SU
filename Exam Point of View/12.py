641aaaf4bddc17ee222e585432ea9f3bdeeca2d7
abhishekkandel45 <38427430+abhishekkandel45@users.noreply.github.com>
2022-05-04 23:19:51 +0530

    Update 12.py

---
+++ Exam Point of View/12.py
@@ -0,0 +1,6 @@
+# write a Python Program to accept the name from the user  and verify wheater the user is authroized to enter the exam or not.
+a= input("Enter your name: ")
+if a == "Rajesh":
+    print("You are authorized to enter the exam")
+else:
+    print("You are not authorized to enter the exam")
