d6c8941795a600061868bcaf1c7ee1bff5cb613b
abhishekkandel45 <38427430+abhishekkandel45@users.noreply.github.com>
2022-05-07 00:31:53 +0530

    Delete Q12.py

--- Lab3/Q12.py
+++
@@ -1,8 +0,0 @@
-# Write a python program to accept a name from the user and verify whether the name is authorized or not?
-# The name should be in the list of authorized names.
-a=input("Enter a name: ")
-b=["John","Mary","Bob","Alice"]
-if a in b:
-    print ("Authorized")
-else:
-    print ("Unauthorized")
