84218e4d9c2e56c487f15aac1beb040b7b25e19c
abhishekkandel45 <38427430+abhishekkandel45@users.noreply.github.com>
2022-05-07 02:24:12 +0530

    Update Q50.py

--- Exam Point of View/Q50.py
+++ Exam Point of View/Q50.py
@@ -1,6 +1,15 @@
 # Python program to change Numbers into Words
-a=int(input("Enter a number: "))
+a=(input("Enter a number: "))
 b={1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",0:"Zero"}
-while a>0:
-    print (b[a%10])
-    a=a//10
+#Seprate the number into a list
+a=list(a)
+#Create a new list
+c=[]
+#Loop through the list
+for i in a:
+    #Change the number into words
+    c.append(b[int(i)])
+#Print the list
+print (c)
+#Print the words
+print (" ".join(c))
