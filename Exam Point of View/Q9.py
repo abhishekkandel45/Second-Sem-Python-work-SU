f10849c5cf61459b0665925926dad1dfbed2ff59
abhishekkandel45 <38427430+abhishekkandel45@users.noreply.github.com>
2022-05-07 00:00:07 +0530

    Create Q9.py

---
+++ Exam Point of View/Q9.py
@@ -0,0 +1,13 @@
+# Write a python code to demostrate the type conversion using int(), float(), str()?
+# Type conversion is the process of converting one data type to another data type.
+# int() converts a string to an integer
+# float() converts a string to a floating point number
+# str() converts a number to a string
+
+
+a=int(input("Enter a number: "))
+b=float(input("Enter a number: "))
+c=str(input("Enter a number: "))
+print (a,b,c)
+print (type(a),type(b),type(c))
+
