f1b4cb99d20972ff3d49143b47cc8c51347e95c2
abhishekkandel45 <38427430+abhishekkandel45@users.noreply.github.com>
2022-05-07 00:50:02 +0530

    Create q29.py

---
+++ Exam Point of View/q29.py
@@ -0,0 +1,5 @@
+# Write a python program  that will accept the base and height of a triangle and will print the area of the triangle?
+b=int(input("Enter the base: "))
+h=int(input("Enter the height: "))
+area=b*h/2
+print ("Area of the triangle is: ",area)
