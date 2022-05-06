742824025f151f452e9230168178f7b04945e0b0
abhishekkandel45 <38427430+abhishekkandel45@users.noreply.github.com>
2022-05-07 00:31:46 +0530

    Create Q13.py

---
+++ Exam Point of View/Q13.py
@@ -0,0 +1,9 @@
+# Write a python program tp find the biggest of two numbers using the condithional if statement?
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+if a>b:
+    print (a)
+if b>a:
+    print (b)
+if a==b:
+    print ("Both are equal")
