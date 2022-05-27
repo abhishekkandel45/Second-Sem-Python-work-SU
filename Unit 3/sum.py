8f70f297effe3e9477170d5fbd75591bd9feb37e
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-05-27 18:08:00 +0530

    Create sum.py

---
+++ Unit 3/sum.py
@@ -0,0 +1,9 @@
+def addnum():
+    a=int(input("Enter the number:"))
+    b= int(input("Enter the number"))
+    addition= a+b
+    print ("The sum of numbers is:",addition)
+
+    #function call
+addnum()
+
