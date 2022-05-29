eaa0c40e3f86ff402a0df17c7bf3fbc6e8ec3857
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-05-29 22:01:51 +0530

    Create Q2A.py

---
+++ Assignment III/Q2A.py
@@ -0,0 +1,9 @@
+#Demostrate the use of Argument and Parameter.  
+#Argument is the value that is passed to the function.
+#Parameter is the variable that is used to store the value of the argument.
+
+def add(a,b):
+    c=a+b
+    print("The sum is",c)
+add(10,20)
+add(20,30)
