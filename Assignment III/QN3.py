ce664a157a06b56e4336a12e940cc2a1ed7d6068
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-05-29 22:02:30 +0530

    Create QN3.py

---
+++ Assignment III/QN3.py
@@ -0,0 +1,8 @@
+#Demostration of built-in function pow() and math.pow()
+import math
+# returns the 2 to the power of 3 in a float
+print (math.pow(2,3))
+# returns the 2 to the power of 3 in an integer
+print (pow(2,3))
+#Returns 2 to the power of 3 in a integer and then do modulo of 3 to get the remainder
+print (pow(2,3,3))
