ee9d96c5b10775eb9974af4b70f5e7c4e9801890
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-06-08 16:44:28 +0530

    Update exp2.py

--- Lab Assignment/Week10/exp2.py
+++ Lab Assignment/Week10/exp2.py
@@ -1,11 +1,12 @@
 # Write a program to demosntrate inheritance in python. it should keep students Detail.
 class Student:
     pass
-    def __init__(self,name,age,marks):
-        self.name=name
-        self.age=age
-        self.marks=marks
-    def display(self):
-        print("The name of the student is: ",self.name)
-        print("The age of the student is: ",self.age)
-        print("The marks of the student is: ",self.marks)
+class StudentDetail(Student):
+    pass
+stuDetail=StudentDetail()
+stuDetail.name=input("Enter the name of the student: ")
+stuDetail.age=int(input("Enter the age of the student: "))
+stuDetail.marks=int(input("Enter the marks of the student: "))
+print("The name of the student is: ",stuDetail.name)
+print("The age of the student is: ",stuDetail.age)
+print("The marks of the student is: ",stuDetail.marks)
