2e2e5189253f3b6da3210b75316ced657a55d4cf
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-06-09 23:56:34 +0530

    Create 6.py

---
+++ Assignment IV/6.py
@@ -0,0 +1,8 @@
+# Write a python program to write the content “Python programming” for the existing file?
+f_name=input("Enter the name of the file: ")
+f=open(f_name,"w")
+f.write("Python programming")
+f.close()
+f=open(f_name,"r")
+print(f.read())
+f.close()
