acea7a9d085eaea2b314340ac60e4df4f8c9f714
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-07-12 07:38:25 +0530

    Update recursion.py

--- ESE/recursion.py
+++ ESE/recursion.py
@@ -1,1 +1,19 @@
-recursion.py
+#A four digit integer is entered through keyboard. Write a function to calculate the sum of the four digit number without recursion and using recursion.
+
+# Without recursion
+def sum_without_recursion(num):
+    sum=0
+    while num>0:
+        sum=sum+num%10
+        num=num//10
+    return sum
+
+# With recursion
+def sum_with_recursion(num):
+    if num==0:
+        return 0
+    else:
+        return num%10+sum_with_recursion(num//10)
+
+
+        
