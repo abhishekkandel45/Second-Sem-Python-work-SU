5f37a2a8f86aa3141e2a2226aa7defd1a1f93b43
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-07-12 06:03:14 +0530

    Create fizzbuzz.py

---
+++ ESE/fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+    if n%3==0 and n%5==0:
+        return "FizzBuzz"
+    elif n%3==0:
+        return "Fizz"
+    elif n%5==0:
+        return "Buzz"
+    else:
+        return n
+
+
+a= int(input("Enter the number:"))
+print (fizzbuzz(a))
