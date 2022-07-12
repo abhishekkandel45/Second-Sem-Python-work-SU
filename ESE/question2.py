343c3c993f9d7c3ebe09b3111ec44179f20c81b7
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-07-12 07:38:22 +0530

    Update question2.py

--- ESE/question2.py
+++ ESE/question2.py
@@ -1,9 +1,9 @@
 #Write a program to define function dec_binary(num) to convert the existing decimal  number into its equivalent binary number
 
 def dec_binary(num):
-    if num>1:
-        dec_binary(num//2)
-    print(num%2,end="")
+    while num>0:
+        rem=num%2
+        num=num//2
+        print(rem,end="")
 
-a=int(input("Enter the Decimal number: "))
-dec_binary(a)
+        
