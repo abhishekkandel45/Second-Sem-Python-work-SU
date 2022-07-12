48ac8771e59a9f70afa0ef9ff805f2fd68cfeae8
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-07-12 07:38:11 +0530

    Update factorial.py

--- ESE/factorial.py
+++ ESE/factorial.py
@@ -1,1 +1,12 @@
-factorial.py
+# Factorial using recursion and without recursion
+def factorial_without_recursion(num):
+    if num==0:
+        return 1
+    else:
+        return num*factorial_without_recursion(num-1)
+
+def factorial_with_recursion(num):
+    if num==0:
+        return 1
+    else:
+        return num*factorial_with_recursion(num-1)
